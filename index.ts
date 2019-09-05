e9076e2674efe6b120f91f56358f77c0ca10a4f8
David Griswold <novachild@gmail.com>
2019-09-05 08:15:50 -0300

    initial commit

---
+++ index.ts
@@ -0,0 +1,27 @@
+/// <reference path="global.d.ts" />
+"use strict";
+// DO NOT TOUCH THE ABOVE CODE.
+
+
+function setup() {
+    createCanvas(400, 600);
+    textSize(32);
+    textAlign(CENTER);
+}
+
+function draw() {
+    
+    if (mouseIsPressed) {
+        fill(0);
+        stroke(255);  
+    }
+    else {
+        fill(255);
+        stroke(0);
+    }
+    
+    ellipse(mouseX, mouseY, 80, 80);
+    
+    fill("red");
+    text("Welcome to p5js!",200,50);
+}
